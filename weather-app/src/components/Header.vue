c5a144fbae10e5e9e92ea7554a2ad790662fc242
sonya <ayanaborsch@gmail.com>
2023-02-10 13:42:42 +0600

    added first component

---
+++ weather-app/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <h1>Weather App Header</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+}
+</script>
+
+<style scoped> /* scoped means that these styles are only for this component*/
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  
+  }
+</style>
+
+
